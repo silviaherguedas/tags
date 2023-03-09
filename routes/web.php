60007d3a2d3d1fde95d8964863c03eca5fae292b
Silvia M. Herguedas <silviaherguedas@gmail.com>
2023-03-09 17:58:52 +0100

    Add TagController, Tag model, and route for storing tags, plus test for store method

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome', [ 'tags' => App\Models\Tag::get()]);
 });
+
+Route::post('tags', [\App\Http\Controllers\TagController::class, 'store']);
